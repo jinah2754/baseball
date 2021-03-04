980fcc7fb9d7db64de54bc4d95e2fe3a88861dfe
jinah <jinah2754@gmail.com>
2021-03-04 22:44:50 +0900

    prompt

--- index.js
+++ index.js
@@ -19,3 +19,35 @@ var select = Math.floor(Math.random() * list.length);
 console.log('list', list, 'number', number, 'length', list.length);
 number[i] = list.splice(select, 1)[0];
 }
+
+var count = 0;
+var strike = 0;
+var ball = 0;
+while (count < 10) {
+    var input = prompt('숫자를 입력하세요');
+    var inputArray = input.split('');
+    strike = 0;
+    ball = 0;
+    count++;
+    for (var j = 0; j < 4; j++) {
+        for (var k = 0; k < 4; k++){
+            if(number[j] == inputArray[k]) {
+                if ( j === k) {
+                    strike++;
+                } else {
+                    ball++;
+                }
+                break;
+            }
+        }
+    }
+
+if (strike === 4) {
+    console.log('홈런!!' + (count - 1) + '번 만에 맞추셨습니다');
+    break;
+}   else if (count >= 10) {
+    console.error('시도 횟수를 초과하셨습니다.');
+} else {
+    console.info(inputArray.join('') + ':' + strike + '스트라이크' + ball + '볼');
+ }
+}
