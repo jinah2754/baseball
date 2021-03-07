01714b5ed6abcd0ca8e67fd22665f5d9dfb5e37b
jinah <jinah2754@gmail.com>
2021-03-07 10:49:55 +0900

    every.function

--- index2.js
+++ index2.js
@@ -1,3 +1,15 @@
 var stirng = "aBcD";
 stirng.toUpperCase(); // "ABCD"
 string.toLowerCase(); // "abcd"
+
+var array = [1,3,5,7,9];
+array.every(function(i) {
+    return i % 2 === 1;
+}); //true
+array.every(function(i) {
+    return i < 9;
+}); //false
+
+array.some(function(i) {
+    return i === 9;
+}); //true
